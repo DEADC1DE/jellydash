b2958f4f1ba891d916785a8b7aa62d2076daa497
themartz90 <233140257+themartz90@users.noreply.github.com>
2026-08-22 19:49:23 +0200

    Fix Statistics accuracy

--- tests/StatisticsRangeFrontendTest.php
+++ tests/StatisticsRangeFrontendTest.php
@@ -28,4 +28,18 @@ final class StatisticsRangeFrontendTest extends TestCase
         $this->assertStringContainsString("AppSettings::set('statistics_default_range'", $request);
         $this->assertStringContainsString("requestIs('statistics-default')", $request);
     }
+
+    public function testTrendAndCoverageValuesAreExposedInTheStatisticsMarkup(): void
+    {
+        $template = file_get_contents(TEMPLATES_DIR . '/statistics/index.twig');
+        $stylesheet = file_get_contents(ROOT_DIR . '/public/assets/css/dashboard.css');
+
+        $this->assertIsString($template);
+        $this->assertIsString($stylesheet);
+        $this->assertStringContainsString('aria-label="{{ bar.label }}: {{ bar.value }}"', $template);
+        $this->assertStringContainsString('{{ stats.codecCoverage }}', $template);
+        $this->assertStringContainsString('{{ stats.reasonCoverage }}', $template);
+        $this->assertStringContainsString('width: {{ user.share }}', $template);
+        $this->assertStringContainsString(".stats-trend-bars i {\n    width: min(30px, 100%);\n    min-height: 0;", $stylesheet);
+    }
 }
